abe8555408a047bb3007d4677de3e86d73e7ccb9
Ernad Halilovic <ehalilovic@wayfair.com>
2019-12-13 12:36:13 +0100

    bump

--- plugins/beacon/alerta_beacon.py
+++ plugins/beacon/alerta_beacon.py
@@ -58,6 +58,7 @@ class ServiceIntegration(PluginBase):
             payload['environment'] = alert.environment
             payload['event'] = alert.event
             payload['id'] = alert.id
+            payload['alert'] = alert.tags[0]
 
             LOG.debug('Beacon payload: %s', payload)
         except Exception as e:
